7082489d2524b2ae20e859f5463b2f4df597516f
cnstntn-kndrtv <kk@digt.ru>
2017-11-16 15:01:45 +0300

    trivia

--- utils/test.js
+++ utils/test.js
@@ -6,18 +6,19 @@ let fileName = './public/js/terms-list.js';
 ldf.Logger.setLevel('error');
 
 var fragmentsClient = new ldf.FragmentsClient('http://ldf.kloud.one/redbook');
-let term = 'чествовать';
-term = 'Аховый';
+let term = 'прозорливый';
 
-let q = 
+let q;
+
+q = 
 `
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX lexinfo: <http://www.lexinfo.net/ontology/2.0/lexinfo#>
     PREFIX ontolex: <http://www.w3.org/ns/lemon/ontolex#>
     PREFIX dc: <http://purl.org/dc/terms#>
-
-    SELECT ?id ?meaning ?usage ?partOfSpeech ?canonicalForm
+    
+    SELECT ?id ?meaning ?usage ?partOfSpeech ?canonicalForm ?dateAccepted
     WHERE {
         ?id a ontolex:LexicalEntry ;
             ontolex:writtenRep "${term}"@ru .
@@ -37,7 +38,35 @@ let q =
             ?senseId ontolex:usage ?usageId .
             ?usageId rdf:value ?usage .
         }
+
+        OPTIONAL {
+            ?id dc:dateAccepted ?dateAccepted .
+        }
     }
+    ORDER BY ?dateAccepted
+    # LIMIT 3
+`
+
+q = 
+`
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX lexinfo: <http://www.lexinfo.net/ontology/2.0/lexinfo#>
+    PREFIX ontolex: <http://www.w3.org/ns/lemon/ontolex#>
+    PREFIX dc: <http://purl.org/dc/terms#>
+    
+    SELECT ?id ?wr ?meaning ?usage ?partOfSpeech ?canonicalForm ?dateAccepted
+    WHERE {
+        ?id a ontolex:LexicalEntry ;
+            ontolex:writtenRep ?wr .
+    
+       OPTIONAL {
+            ?id dc:dateAccepted ?dateAccepted .
+        }
+    }
+    # ORDER BY ?dateAccepted
+    # OFFSET 1
+    # LIMIT 3
 `
 
 let r = new ldf.SparqlIterator(q, { fragmentsClient: fragmentsClient });
